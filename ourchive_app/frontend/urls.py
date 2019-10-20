c845aad3ae13273ed59ed5288b36399e87330a34
Elena Palmer <c.elena.palmer@gmail.com>
2019-10-20 14:58:41 -0500

    template placeholders + responsive nav

--- ourchive_app/frontend/urls.py
+++ ourchive_app/frontend/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
 	path('', views.index),
+    path('search/', views.search),
+    path('works/', views.works),
+    path('works/type/<int:type_id>', views.works_by_type),
+    path('works/<int:pk>/', views.work),
+    path('bookmarks/', views.bookmarks),
+    path('bookmark/<int:pk>/', views.bookmark),
 ]
--- ourchive_app/frontend/views.py
+++ ourchive_app/frontend/views.py
@@ -7,5 +7,37 @@ def index(request):
 	work_types = response.json()
 	return render(request, 'index.html', {
 	    'work_types': work_types['results'],
-	    'message': 'Hello! This is some scaffolding to determine effective view architecture. Stay tuned!'
+	    'heading_message': 'Welcome to Ourchive',
+	    'long_message': 'Ourchive is a configurable, extensible, multimedia archive, meant to serve as a modern alternative to PHP-based archives. You can search for existing works, create your own, or create curated collections of works you\'ve enjoyed. Have fun with it!'
 	})
+
+def search(request):
+	response = requests.get(settings.ALLOWED_HOSTS[0] + '/api/worktypes')
+	work_types = response.json()
+	return render(request, 'search.html', {'work_types': work_types['results']})
+
+def works(request):
+	response = requests.get(settings.ALLOWED_HOSTS[0] + '/api/worktypes')
+	work_types = response.json()
+	return render(request, 'works.html', {'work_types': work_types['results']})
+
+def works_by_type(request, type_id):
+	response = requests.get(settings.ALLOWED_HOSTS[0] + '/api/worktypes')
+	work_types = response.json()
+	# todo: get works by type
+	return render(request, 'works.html', {'work_types': work_types['results']})
+
+def work(request, pk):
+	response = requests.get(settings.ALLOWED_HOSTS[0] + '/api/worktypes')
+	work_types = response.json()
+	return render(request, 'work.html', {'work_types': work_types['results']})
+
+def bookmarks(request):
+	response = requests.get(settings.ALLOWED_HOSTS[0] + '/api/worktypes')
+	work_types = response.json()
+	return render(request, 'bookmarks.html', {'work_types': work_types['results']})
+
+def bookmark(request, pk):
+	response = requests.get(settings.ALLOWED_HOSTS[0] + '/api/worktypes')
+	work_types = response.json()
+	return render(request, 'bookmark.html', {'work_types': work_types['results']})
