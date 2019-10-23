07944ebca6898bb168c0be7e5c480b6d6ae7f9d0
Kate Reeher <kate.reeher@Calabrio.com>
2019-10-23 16:59:45 -0500

    OOOH ENDPOINT API ENPOINT

--- tests/tests_api.py
+++ tests/tests_api.py
@@ -1,23 +1,48 @@
+#
 import pytest
-#from api.urls import urlpatterns
 import requests
-#from rest_framework.test import APITestCase
-#from api.models import User
 import os
 import logging
 
 root = 'http://host.docker.internal:8000/'
 
-#def test_01():
 
-#    print(urlpatterns)
-#    assert 2+2 == 4
+def get_endpoints():
 
-@pytest.mark.parametrize("endpoint", ['users', 'works'])
+    url = root + 'api/'
+    response = requests.get(url)
+    urlpatterns = response.json()
+
+    return list(urlpatterns)
+
+
+def test_01():
+
+    url = root + 'api/'
+    response = requests.get(url)
+    urlpatterns = response.json()
+
+    logging.info(list(urlpatterns))
+
+
+@pytest.mark.parametrize("endpoint", get_endpoints())
 def test_api_connect(endpoint):
 
     url = root + 'api/' + endpoint
-    logging.info(url)
+
     response = requests.get(url)
+    logging.info(endpoint)
+    logging.info(response.json())
     assert response.status_code == 200
-    assert len(response.json()) > 0
+    assert response.json()['count'] is not None
+
+
+def test_user_crud():
+
+    url = root + 'api/'
+
+    body = {
+
+    }
+
+    r = requests.post(url, body)
