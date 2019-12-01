240435b09f0403a10ff10aba5243cbeead368b37
Elena Palmer <c.elena.palmer@gmail.com>
2019-12-01 16:22:29 -0600

    update wsgi file

--- ourchive_app/ourchive_app/wsgi.py
+++ ourchive_app/ourchive_app/wsgi.py
@@ -7,7 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 from django.core.wsgi import get_wsgi_application
 
